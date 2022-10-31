a42512108f77ee3e89cd0fb77ae5915d8b1f880b
AzureVauban <haloringman@outlook.com>
2022-10-31 04:32:43 +0000

    Oct 30, 2022, 11:32 PM

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/halor/Programming/Cloned Projects/cop3530-catch2-demo
+# Build directory: C:/Users/halor/Programming/Cloned Projects/cop3530-catch2-demo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
